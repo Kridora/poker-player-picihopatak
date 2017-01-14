2031a241adb3ee2ee61549d986b05bc670a50201
peti01 <kiss.balazs91@gmail.com>
2017-01-14 14:07:59 +0100

    confret fix

--- src/main/java/org/leanpoker/player/config/ConfigRetriever.java
+++ src/main/java/org/leanpoker/player/config/ConfigRetriever.java
@@ -5,19 +5,16 @@ import java.net.URL;
 import java.nio.charset.Charset;
 
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-import com.oracle.javafx.jmx.json.JSONException;
 import org.apache.commons.io.IOUtils;
 import org.leanpoker.player.json.Config;
 
 public class ConfigRetriever {
     static final String url = "http://owlab.hu/conf.txt";
     
-    public static Config readJsonFromUrl() throws IOException, JSONException {
+    public static Config readJsonFromUrl() throws IOException {
         String jsonText = IOUtils.toString(new URL(url), Charset.defaultCharset());
         return new GsonBuilder().create().fromJson(jsonText, Config.class);
     }
-        
+
     
 }
