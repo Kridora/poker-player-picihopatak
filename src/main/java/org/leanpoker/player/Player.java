f937325a08ffce63ad00dc1f112cbf978be3387d
csaron92 <csaron92@gmail.com>
2017-01-14 15:41:12 +0100

    stage1

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -21,11 +21,24 @@ public class Player {
         GameState gameState = new GsonBuilder().create().fromJson(request, GameState.class);
         DoWeHave doWeHave = new DoWeHave();
         
+        /* load config */
         if(gameState.getRound() == 0){
         	config = ConfigRetriever.readJsonFromUrl();
         }
+        
+        Integer bbLimit = Integer.parseInt(config.getALL_IN_LIMIT_IN_BB());
       
         if (config != null && config.getKEEP_POCKET_PAIRS().equals("ON")) {
+        	if (bbLimit > 0){
+        		if(bbLimit*gameState.getSmall_blind()*2< gameState.getMe().getStack()  ){
+        			/*premium lap*/
+        			if (doWeHave.premiumCards(gameState.getMe().getHole_cards())) {
+            			return 20000;						
+					}
+        			else
+        				return 0;        			
+        		}
+        	}
             return new Strategy().getBet(doWeHave, gameState);
         }
         return 0;
--- src/main/java/org/leanpoker/player/json/Config.java
+++ src/main/java/org/leanpoker/player/json/Config.java
@@ -3,12 +3,18 @@ package org.leanpoker.player.json;
 
 public class Config {
     private String KEEP_POCKET_PAIRS;
+    private String ALL_IN_LIMIT_IN_BB;
 
     public Config(String KEEP_POCKET_PAIRS) {
         this.KEEP_POCKET_PAIRS = KEEP_POCKET_PAIRS;
+        this.ALL_IN_LIMIT_IN_BB = ALL_IN_LIMIT_IN_BB;
     }
 
     public String getKEEP_POCKET_PAIRS() {
         return KEEP_POCKET_PAIRS;
+    }
+
+    public String getALL_IN_LIMIT_IN_BB() {
+        return ALL_IN_LIMIT_IN_BB;
     }
 }
